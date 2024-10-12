ee8d8459119449a7c36a9746362dd540c3450ded
Di-Zi <dima-zimin@mail.ru>
2024-10-12 21:48:25 +0300

    lesson 1 base syntax

--- main.py
+++ main.py
@@ -7,10 +7,43 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print('Hi, ' + name)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Roman')
+    print_hi('Dim')
+
+    number = 100
+    float_number = 10.5
+    text = 'text "example"'
+    text2 = "text \"example\""
+    boolean_value = True
+    # a, b, c = 20, 10, 56
+    values = [10, 'number', 4, 9]
+    dictionary = {}
+
+    a = {1,2,3,4,5}
+    b = set('hello stars')
+    c = set([1,2,4,6,8])
+
+    # d = 7
+    # count = 0
+    # while d>0:
+    #     count = count + 1
+    #     d = d-1
+    #     print (f'count {count}')
+    #     print (d)
+    #     if count == 3:
+    #         print ('number 3')
+    #         continue
+    #     if count == 5:
+    #         print ('number 5')
+    #         break
+
+
+    # print (b, type(b))
+    # print (c, type(c))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
